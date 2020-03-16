d9a789ec23179331eedb5b3da0cee904b199fe6c
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2020-03-16 17:07:30 +0100

    utilizzo pipe e safenavigator

--- src/app/primo/primo.component.ts
+++ src/app/primo/primo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primo',
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./primo.component.scss']
 })
 export class PrimoComponent implements OnInit {
+  title = 'DemoBergamo';
 
+  htmlString='<div><p>Prova</p></div>';
+  linkUrl='https://google.it';
+ @Input()
+ titoloInput : string;
+lista=[1,2,5,6];
+oggetto: {chiave:string};
+oggetto1: {chiave:string}={
+  chiave:'valore'
+};
+myDate : Date=new Date();
   constructor() { }
 
   ngOnInit(): void {
